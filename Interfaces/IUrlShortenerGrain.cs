80aaeacaf051f89ba606cb21196a993a7d678434
Caio Vinícius Meneses Silva <caio.silva@xogito.com>
2023-11-07 16:01:10 -0300

    Add project files.

---
+++ Interfaces/IUrlShortenerGrain.cs
@@ -0,0 +1,9 @@
+﻿namespace OrleansURLShortener.Interfaces
+{
+    public interface IUrlShortenerGrain : IGrainWithStringKey
+    {
+        Task SetUrl(string fullUrl);
+
+        Task<string> GetUrl();
+    }
+}
---
+++ Models/UrlShortenerGrain.cs
@@ -0,0 +1,40 @@
+﻿using Orleans.Runtime;
+using OrleansURLShortener.Interfaces;
+
+namespace OrleansURLShortener.Models
+{
+    public sealed class UrlShortenerGrain : Grain, IUrlShortenerGrain
+    {
+        private readonly IPersistentState<UrlDetails> _state;
+
+        public UrlShortenerGrain(
+            [PersistentState(
+            stateName: "url",
+            storageName: "urls")]
+            IPersistentState<UrlDetails> state) => _state = state;
+
+        public async Task SetUrl(string fullUrl)
+        {
+            _state.State = new()
+            {
+                ShortenedRouteSegment = this.GetPrimaryKeyString(),
+                FullUrl = fullUrl
+            };
+
+            await _state.WriteStateAsync();
+        }
+
+        public Task<string> GetUrl() =>
+            Task.FromResult(_state.State.FullUrl);
+    }
+
+    [GenerateSerializer]
+    public record class UrlDetails
+    {
+        [Id(0)]
+        public string FullUrl { get; set; }
+
+        [Id(1)]
+        public string ShortenedRouteSegment { get; set; }
+    }
+}
